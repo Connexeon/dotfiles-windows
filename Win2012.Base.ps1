f1aac98135c7bd28be4a0317d4559b217d023eb0
Hannes Van de Vel <h@nnes.be>
2017-05-31 02:11:28 +0200

    Adding Windows 2012 Server base

---
+++ Win2012.Base.ps1
@@ -0,0 +1,53 @@
+# The following settings will ask you for your windows password and then
+# successfuly reboot the machine everytime it needs to. After Boxstarter is
+# done autologin won't be enabled.
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$false # Save my password securely and auto-login after a reboot
+
+# Allow running PowerShell scripts
+Update-ExecutionPolicy Unrestricted
+
+# Remote access / support
+Enable-RemoteDesktop
+
+# Install Windows Update and reboot
+Install-WindowsUpdate -acceptEula
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# Power Options - disable hibernation and disable monitor standby
+Write-Host "Configuring power options..."
+powercfg -change -monitor-timeout-ac 0
+powercfg -change -standby-timeout-ac 0
+powercfg -h off
+
+Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+
+# Install terminal font for RDM
+$FONTS = 0x14
+$objShell = New-Object -ComObject Shell.Application
+$objFolder = $objShell.Namespace($FONTS)
+$tempFile = $env:TEMP + "\DejaVu Sans Mono for Powerline.ttf"
+(new-object System.Net.WebClient).DownloadFile('http://connexeon.link/download-rdm-font', $tempFile);
+$objFolder.CopyHere($tempFile);
+rm $tempFile -rec
+
+#region Chocolatey all the way
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n=allowEmptyChecksums
+choco feature enable -n=allowGlobalConfirmation
+#endregion
+
+#region DotNetAndPowershell
+cinst PowerShell
+cinst DotNet4.0
+cinst DotNet4.5
+cinst DotNet4.5.1
+#endregion
+
+#region Runtimes
+cinst javaruntime
+cinst java.jdk
+cinst python
+cinst scriptcs
+#endregion
