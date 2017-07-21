d7f1fba08694babc55a23f2f8a7193f91e92f9f2
Hannes Van de Vel <h@nnes.be>
2017-07-22 00:18:10 +0200

    Update win10-survavilkit.ps1

--- win10-survavilkit.ps1
+++ win10-survavilkit.ps1
@@ -1,9 +1,9 @@
 # The following settings will ask you for your windows password and then
 # successfuly reboot the machine everytime it needs to. After Boxstarter is
 # done autologin won't be enabled.
-$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.RebootOk=$false # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
-$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+$Boxstarter.AutoLogin=$false # Save my password securely and auto-login after a reboot
 
 # Allow running PowerShell scripts
 Update-ExecutionPolicy Unrestricted
