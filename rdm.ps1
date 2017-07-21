2bd17fc4b6ec41ee7354e34d501d988943a24a88
Hannes Van de Vel <h@nnes.be>
2017-07-22 00:48:43 +0200

    Create rdm.ps1

---
+++ rdm.ps1
@@ -0,0 +1,20 @@
+# RDM
+Start-Process msiexec.exe -Wait -ArgumentList '/I https://connexeon.link/download-rdm-beta-msi /quiet'
+
+# Install terminal font for RDM
+$FONTS = 0x14
+$objShell = New-Object -ComObject Shell.Application
+$objFolder = $objShell.Namespace($FONTS)
+$tempFile = $env:TEMP + "\DejaVu Sans Mono for Powerline.ttf"
+(new-object System.Net.WebClient).DownloadFile('http://connexeon.link/download-rdm-font', $tempFile);
+$objFolder.CopyHere($tempFile);
+rm $tempFile -rec
+
+# Pin to taskbar
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Devolutions\Remote Desktop Manager\RemoteDesktopManager64.exe"
+
+# Configure Connexeon RDM datasource
+Import-Module "${env:ProgramFiles(x86)}\Devolutions\Remote Desktop Manager\RemoteDesktopManager.PowerShellModule.dll"
+$s = New-RDMDataSource -SQLServer -Database ConnexeonRDM -IntegratedSecurity -Server rdm.connexeon.local -Name Connexeon -SetDatasource
+Set-RDMDatasource $s
+
