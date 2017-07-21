baf8a85978fe4b17396e9d1bbbb4063b17046ba0
Hannes Van de Vel <h@nnes.be>
2017-07-21 14:57:34 +0200

    Adding ZeroTier

--- Win10.Connexeon.Base.ps1
+++ Win10.Connexeon.Base.ps1
@@ -29,6 +29,9 @@ Start-Process msiexec.exe -Wait -ArgumentList '/I http://downloads.3cx.com/downl
 # Splashtop Streamer configured with deploy code
 Start-Process msiexec.exe -Wait -ArgumentList '/norestart /qn /i https://d17kmd0va0f0mp.cloudfront.net/csrs/Splashtop_Streamer_Windows_deploy_v3.1.4.1.msi USERINFO="dcode=4R2WY2KJ5Z5A,hidewindow=1"'
 
+# ZeroTier One
+Start-Process msiexec.exe -Wait -ArgumentList '/I https://download.zerotier.com/dist/ZeroTier%20One.msi /quiet'
+
 # Dev / Doc Tools
 cinst git
 cinst github-desktop
