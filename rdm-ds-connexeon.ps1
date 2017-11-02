8db70792619bd22094a30b943d658c1bab0d1d36
Hannes Van de Vel <h@nnes.be>
2017-11-02 21:46:53 +0100

    Update rdm-ds-connexeon.ps1

--- rdm-ds-connexeon.ps1
+++ rdm-ds-connexeon.ps1
@@ -1,4 +1,6 @@
 # Configure Connexeon RDM datasource (Zerotier)
+#  Use like this from IE or Edge (or with OneClick plugin in Chrome/Firefox) 
+#  http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/Connexeon/dotfiles-windows/master/rdm-ds-connexeon.ps1
 
 Import-Module "${env:ProgramFiles(x86)}\Devolutions\Remote Desktop Manager\RemoteDesktopManager.PowerShellModule.dll"
 $s = New-RDMDataSource -SQLServer -Database ConnexeonRDM -IntegratedSecurity -Server 10.244.0.15 -Name Connexeon -SetDatasource
