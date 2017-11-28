7a7d42f0e28618b2b0e28b3401b30c2d47acbf98
Hannes Van de Vel <h@nnes.be>
2017-11-28 21:35:47 +0100

    Update rdm.ps1

--- rdm.ps1
+++ rdm.ps1
@@ -1,15 +1,9 @@
 # RDM
 Start-Process msiexec.exe -Wait -ArgumentList '/I https://connexeon.link/download-rdm-beta-msi /quiet'
 
-# Install terminal font for RDM
-$FONTS = 0x14
-$objShell = New-Object -ComObject Shell.Application
-$objFolder = $objShell.Namespace($FONTS)
-$tempFile = $env:TEMP + "\DejaVu Sans Mono for Powerline.ttf"
-(new-object System.Net.WebClient).DownloadFile('https://connexeon.link/download-rdm-font', $tempFile);
-$objFolder.CopyHere($tempFile);
-rm $tempFile -rec
+# Install terminal fonts
+choco install inconsolata -y
 
 # Pin to taskbar
-Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Devolutions\Remote Desktop Manager\RemoteDesktopManager.exe"
+Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles}\Devolutions\Remote Desktop Manager\RemoteDesktopManager.exe"
 
