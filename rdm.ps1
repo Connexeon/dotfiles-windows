c0904133ca591cfe1d9dd82f9382f8d7547bc840
Hannes Van de Vel <h@nnes.be>
2018-02-04 01:49:51 +0100

    Update rdm.ps1

--- rdm.ps1
+++ rdm.ps1
@@ -4,6 +4,7 @@ Start-Process msiexec.exe -Wait -ArgumentList '/I https://connexeon.link/downloa
 # Install terminal fonts
 choco install inconsolata -y
 choco install SourceCodePro -y
+choco install dejavufonts -y
 
 # Pin to taskbar
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles}\Devolutions\Remote Desktop Manager\RemoteDesktopManager.exe"
