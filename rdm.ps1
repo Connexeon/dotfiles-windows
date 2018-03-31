875aec1b263b69fe6c86a79dfaa19a254aa5b4b3
Hannes Van de Vel <h@nnes.be>
2018-03-31 23:26:13 +0200

    Update rdm.ps1
    Adding Powerline fonts install from git

--- rdm.ps1
+++ rdm.ps1
@@ -2,9 +2,11 @@
 Start-Process msiexec.exe -Wait -ArgumentList '/I https://connexeon.link/download-rdm-beta-msi /quiet'
 
 # Install terminal fonts
-choco install inconsolata -y
-choco install SourceCodePro -y
-choco install dejavufonts -y
+choco install git -y
+cd $HOME\Downloads
+git clone https://github.com/powerline/fonts.git powerline-fonts
+cd $HOME\Downloads\powerline-fonts
+.\install.ps1
 
 # Pin to taskbar
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles}\Devolutions\Remote Desktop Manager\RemoteDesktopManager.exe"
