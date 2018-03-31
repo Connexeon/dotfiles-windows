e65c30b2cda7f8967933a2fd6289d24392d55e5f
Hannes Van de Vel <h@nnes.be>
2018-03-31 23:49:29 +0200

    Update rdm.ps1
    Update to overwrite existing fonts silently

--- rdm.ps1
+++ rdm.ps1
@@ -6,7 +6,24 @@ choco install git -y
 cd $HOME\Downloads
 git clone https://github.com/powerline/fonts.git powerline-fonts
 cd $HOME\Downloads\powerline-fonts
-.\install.ps1
+
+$FontName = '*'
+$fontFiles = New-Object 'System.Collections.Generic.List[System.IO.FileInfo]'
+foreach ($aFontName in $FontName) {
+    Get-ChildItem $PSScriptRoot -Filter "${aFontName}.ttf" -Recurse | Foreach-Object {$fontFiles.Add($_)}
+    Get-ChildItem $PSScriptRoot -Filter "${aFontName}.otf" -Recurse | Foreach-Object {$fontFiles.Add($_)}
+}
+
+$fonts = $null
+foreach ($fontFile in $fontFiles) {
+    if ($PSCmdlet.ShouldProcess($fontFile.Name, "Install Font")) {
+        if (!$fonts) {
+            $shellApp = New-Object -ComObject shell.application
+            $fonts = $shellApp.NameSpace(0x14)
+        }
+        
+        $fonts.CopyHere($fontFile.FullName, 16)
+    }
 
 # Pin to taskbar
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles}\Devolutions\Remote Desktop Manager\RemoteDesktopManager.exe"
