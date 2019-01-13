a05ffe2e2e822e0d6f6b51006b24dcedd0831c32
Hannes Van de Vel <h@nnes.be>
2019-01-13 14:35:42 +0100

    Create splashtop.ps1

---
+++ splashtop.ps1
@@ -0,0 +1,2 @@
+# Install Splashtop Streamer Deployment
+Start-Process msiexec.exe -Wait -ArgumentList '/norestart /qn /I https://connexeon.link/download-splashtop-streamer-msi USERINFO="dcode=4R2WY2KJ5Z5A,hidewindow=1"'
