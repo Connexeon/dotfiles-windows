ff6f3d75f9ab3fecb30abd2027c32edeed086ca7
Hannes Van de Vel <h@nnes.be>
2019-01-15 19:59:25 +0100

    Update splashtop.ps1

--- splashtop.ps1
+++ splashtop.ps1
@@ -1,2 +1,5 @@
+# Install launching  this link from Edge / IE : 
+#   http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/Connexeon/dotfiles-windows/master/splashtop.ps1
+
 # Install Splashtop Streamer Deployment
 Start-Process msiexec.exe -Wait -ArgumentList '/norestart /qn /I https://connexeon.link/download-splashtop-streamer-msi USERINFO="dcode=4R2WY2KJ5Z5A,hidewindow=1"'
