4af899710baa8844d9c331e60a6cfe423fc952d0
Hannes Van de Vel <h@nnes.be>
2019-01-24 06:00:25 +0100

    Update win10-ops.ps1

--- win10-ops.ps1
+++ win10-ops.ps1
@@ -1,3 +1,5 @@
+# START http://boxstarter.org/package/nr/url?https://connexeon.link/install-win10-ops
+
 # The following settings will ask you for your windows password and then
 # successfuly reboot the machine everytime it needs to. After Boxstarter is
 # done autologin won't be enabled.
@@ -5,8 +7,21 @@ $Boxstarter.RebootOk=$false # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$false # Save my password securely and auto-login after a reboot
 
-# Remote Desktop Manager
-Install-BoxstarterPackage 'http://connexeon.link/install-rdm'
+Write-Host "Win Settings"
+
+Disable-BingSearch
+
+Disable-GameBarTips
+
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableOpenFileExplorerToQuickAccess -EnableShowRecentFilesInQuickAccess -EnableShowFrequentFoldersInQuickAccess -EnableExpandToOpenFolder
+
+Set-TaskbarOptions -Size Large -Dock Bottom -Combine Always -AlwaysShowIconsOn -Lock
+
+Update-ExecutionPolicy Unrestricted -Force
+
+Enable-RemoteDesktop
+
+Disable-InternetExplorerESC
 
 # Create AppModelUnlock if it doesn't exist, required for enabling Developer Mode
 $RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
@@ -17,14 +32,19 @@ if (-not(Test-Path -Path $RegistryKeyPath)) {
 # Add registry value to enable Developer Mode
 New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
 
+# Remote Desktop Manager
+Write-Host "Remote Desktop Manager"
+Install-BoxstarterPackage 'http://connexeon.link/install-rdm'
+
 # Ubuntu Windows
-lxrun /install /y
+#lxrun /install /y
 
 # NuGet 
 Install-PackageProvider -Name "nuget" -Force
 cinst NuGet.CommandLine
 
 # More Ops software & tools 
+Write-Host "More software"
 cinst Microsoft-Hyper-V-All -source windowsfeatures
 cinst HeidiSQL
 cinst sql-server-management-studio
@@ -35,17 +55,18 @@ cinst treesizefree
 cinst winscp
 cinst putty
 #cinst axel
+cinst sysinternals 
+cinst git
+cinst powershell
+cinst chocolatey
+cinst ChocolateyGUI
+cinst 7zip.install
+cinst dotnetfx
+cinst Microsoft-Windows-Subsystem-Linux -source windowsfeatures -y
 cinst windowsazurepowershell
 cinst virtualbox
 #cinst cygwin
-
 cinst curl
-#cinst sourcetree
-#cinst vagrant
-#cinst packer
-#cinst cmder
-#cinst boot2docker
-
 #region BoxStarter baby
 cinst boxstarter
 cinst Boxstarter.Azure
@@ -67,14 +88,169 @@ cinst wireshark
 cinst treesizefree
 cinst winscp
 cinst putty
-cinst axel
 cinst ccleaner
 cinst ccenhancer
-cinst rambox
+#cinst rambox
 #endregion
 
 #region Webbrowsers
 cinst AllBrowsers
 cinst elinks
-cinst tor-browser
+#cinst tor-browser
 #endregion
+
+#--- Uninstall unecessary applications that come with Windows out of the box ---
+
+Write-Host "Uninstall stuff"
+# 3D Builder
+Get-AppxPackage Microsoft.3DBuilder | Remove-AppxPackage
+
+# Alarms
+Get-AppxPackage Microsoft.WindowsAlarms | Remove-AppxPackage
+
+# Autodesk
+Get-AppxPackage *Autodesk* | Remove-AppxPackage
+
+# Bing Weather, News, Sports, and Finance (Money):
+Get-AppxPackage Microsoft.BingFinance | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingSports | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
+
+# BubbleWitch
+Get-AppxPackage *BubbleWitch* | Remove-AppxPackage
+
+# Candy Crush
+Get-AppxPackage king.com.CandyCrush* | Remove-AppxPackage
+
+# Comms Phone
+Get-AppxPackage Microsoft.CommsPhone | Remove-AppxPackage
+
+# Dell
+Get-AppxPackage *Dell* | Remove-AppxPackage
+
+# Dropbox
+Get-AppxPackage *Dropbox* | Remove-AppxPackage
+
+# Facebook
+Get-AppxPackage *Facebook* | Remove-AppxPackage
+
+# Feedback Hub
+Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
+
+# Get Started
+Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
+
+# Keeper
+Get-AppxPackage *Keeper* | Remove-AppxPackage
+
+# Mail & Calendar
+Get-AppxPackage microsoft.windowscommunicationsapps | Remove-AppxPackage
+
+# Maps
+Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
+
+# March of Empires
+Get-AppxPackage *MarchofEmpires* | Remove-AppxPackage
+
+# McAfee Security
+Get-AppxPackage *McAfee* | Remove-AppxPackage
+
+# Messaging
+Get-AppxPackage Microsoft.Messaging | Remove-AppxPackage
+
+# Minecraft
+Get-AppxPackage *Minecraft* | Remove-AppxPackage
+
+# Netflix
+Get-AppxPackage *Netflix* | Remove-AppxPackage
+
+# Office Hub
+Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+
+# One Connect
+Get-AppxPackage Microsoft.OneConnect | Remove-AppxPackage
+
+# OneNote
+Get-AppxPackage Microsoft.Office.OneNote | Remove-AppxPackage
+
+# People
+Get-AppxPackage Microsoft.People | Remove-AppxPackage
+
+# Phone
+Get-AppxPackage Microsoft.WindowsPhone | Remove-AppxPackage
+
+# Photos
+Get-AppxPackage Microsoft.Windows.Photos | Remove-AppxPackage
+
+# Plex
+Get-AppxPackage *Plex* | Remove-AppxPackage
+
+# Skype (Metro version)
+Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
+
+# Sound Recorder
+Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage
+
+# Solitaire
+Get-AppxPackage *Solitaire* | Remove-AppxPackage
+
+# Sticky Notes
+Get-AppxPackage Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
+
+# Sway
+Get-AppxPackage Microsoft.Office.Sway | Remove-AppxPackage
+
+# Twitter
+Get-AppxPackage *Twitter* | Remove-AppxPackage
+
+# Xbox
+Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage
+
+# Zune Music, Movies & TV
+Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage
+
+Write-Host "more win stuff"
+#--- Windows Settings ---
+# Some from: @NickCraver's gist https://gist.github.com/NickCraver/7ebf9efbfd0c3eab72e9
+
+# WiFi Sense: HotSpot Sharing: Disable
+If (-Not (Test-Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
+    New-Item -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting | Out-Null
+}
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting -Name value -Type DWord -Value 0
+
+# WiFi Sense: Shared HotSpot Auto-Connect: Disable
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots -Name value -Type DWord -Value 0
+
+# Start Menu: Disable Bing Search Results
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
+# To Restore (Enabled):
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 1
+
+# Disable Telemetry (requires a reboot to take effect)
+# Note this may break Insider builds for your organization
+# Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection -Name AllowTelemetry -Type DWord -Value 0
+# Get-Service DiagTrack,Dmwappushservice | Stop-Service | Set-Service -StartupType Disabled
+
+# Change Explorer home screen back to "This PC"
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 1
+# Change it back to "Quick Access" (Windows 10 default)
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 2
+
+# Better File Explorer
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1		
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1		
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+# Disable Xbox Gamebar
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" -Name AppCaptureEnabled -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name GameDVR_Enabled -Type DWord -Value 0
+
+# Turn off People in Taskbar
+If (-Not (Test-Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People")) {
+    New-Item -Path HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People | Out-Null
+}
+Set-ItemProperty -Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name PeopleBand -Type DWord -Value 0
